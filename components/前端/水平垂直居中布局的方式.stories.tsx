c2f49a2604dc653fd84794d242781cd38aafb0d1
yarnb <yarnbcoder@gmail.com>
2020-04-20 19:25:17 +0800

    docs: update

--- components/前端/水平垂直居中布局的方式.stories.tsx
+++ components/前端/水平垂直居中布局的方式.stories.tsx
@@ -22,7 +22,7 @@ export const Flex = () => {
     justify-content: center;
   `;
   const ChildOfImg = styled.img.attrs(() => ({
-    src: require('../assets/hzw.jpeg'),
+    src: require('../../assets/hzw.jpeg'),
   }))`
     width: 100px;
     height: ${imgHeight}px;
@@ -88,7 +88,7 @@ export const LineHeightAndVerticalAlign = () => {
     text-align: center;
   `;
   const ChildOfImg = styled.img.attrs(() => ({
-    src: require('../assets/hzw.jpeg'),
+    src: require('../../assets/hzw.jpeg'),
   }))`
     width: 100px;
     height: ${imgHeight}px;
@@ -135,7 +135,7 @@ export const VerticalAlign = () => {
     text-align: center;
   `;
   const ChildOfImg = styled.img.attrs(() => ({
-    src: require('../assets/hzw.jpeg'),
+    src: require('../../assets/hzw.jpeg'),
   }))`
     width: 100px;
     height: ${imgHeight}px;
@@ -214,7 +214,7 @@ export const TableCell = () => {
   `;
 
   const ChildOfImg = styled.img.attrs(() => ({
-    src: require('../assets/hzw.jpeg'),
+    src: require('../../assets/hzw.jpeg'),
   }))`
     width: 100px;
     height: ${imgHeight}px;
@@ -281,7 +281,7 @@ export const AbsoluteMargin = () => {
   `;
 
   const ChildOfImg = styled.img.attrs(() => ({
-    src: require('../assets/hzw.jpeg'),
+    src: require('../../assets/hzw.jpeg'),
   }))`
     width: 100px;
     height: ${imgHeight}px;
@@ -352,7 +352,7 @@ export const AbsoluteMargin2 = () => {
   `;
 
   const ChildOfImg = styled.img.attrs(() => ({
-    src: require('../assets/hzw.jpeg'),
+    src: require('../../assets/hzw.jpeg'),
   }))`
     width: 100px;
     height: ${imgHeight}px;
@@ -423,7 +423,7 @@ export const AbsoluteTransform = () => {
   `;
 
   const ChildOfImg = styled.img.attrs(() => ({
-    src: require('../assets/hzw.jpeg'),
+    src: require('../../assets/hzw.jpeg'),
   }))`
     width: 100px;
     height: ${imgHeight}px;
